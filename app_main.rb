8ed65625c600d5a1384f9db3b3ffaf05f79da022
Takamasa Mizukami <mizukami2005@gmail.com>
2017-09-17 22:48:10 +0900

    create carousel

--- app_main.rb
+++ app_main.rb
@@ -8,7 +8,7 @@ end
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    config.channel_token  = ENV["LINE_CHANNEL_TOKEN"]
   }
 end
 
@@ -17,7 +17,9 @@ post '/callback' do
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
+    error 400 do
+      'Bad Request'
+    end
   end
 
   events = client.parse_events_from(body)
@@ -26,14 +28,67 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        message = {
+        message  = {
           type: 'text',
           text: event.message['text']
         }
-        client.reply_message(event['replyToken'], message)
+        carousel = {
+          type:     'template',
+          altText:  'this is a carousel template',
+          template: {
+            type:    'carousel',
+            columns: [
+                       {
+                         thumbnailImageUrl: 'https://h-navi.jp/uploads/support_facility/157092/half_0fa2da42-6df4-4f2c-83fa-cab44ea7f95c.png',
+                         title:             'でらいとわーくジュニア　かまたアフタースクール',
+                         text:              '放課後等デイサービス',
+                         actions:           [
+                                              {
+                                                type:  'postback',
+                                                label: "Buy",
+                                                data:  "action=buy&itemid=111"
+                                              },
+                                              {
+                                                type:  'uri',
+                                                label: '電話をかける',
+                                                data:  '000-0000-0000'
+                                              },
+                                              {
+                                                type:  'uri',
+                                                label: "詳しく見る",
+                                                uri:   'https://h-navi.jp/support_facility/facilities/157092'
+                                              }
+                                            ]
+                       },
+                       {
+                         thumbnailImageUrl: 'https://h-navi.jp/uploads/support_facility/157161/half_6737b4bc-23ec-4303-a74f-515a9f5deb06.png',
+                         title:             '発達支援教室にじいろLabo池上',
+                         text:              '放課後等デイサービス',
+                         actions:           [
+                                              {
+                                                type:  'postback',
+                                                label: "Buy",
+                                                data:  "action=buy&itemid=111"
+                                              },
+                                              {
+                                                type:  'uri',
+                                                label: '電話をかける',
+                                                data:  '000-0000-0000'
+                                              },
+                                              {
+                                                type:  'uri',
+                                                label: "詳しく見る",
+                                                uri:   'https://h-navi.jp/support_facility/facilities/157161'
+                                              }
+                                            ]
+                       }
+                     ]
+          }
+        }
+        client.reply_message(event['replyToken'], carousel)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
+        tf       = Tempfile.open("content")
         tf.write(response.body)
       end
     end
